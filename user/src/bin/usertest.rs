0a5b02d6cb69f172b199fdc277bba3557894c54c
molinwu <858459615@qq.com>
2025-05-27 14:28:55 +0800

    ✅ chore:修改测试文件，便于测试

--- user/src/bin/usertest.rs
+++ user/src/bin/usertest.rs
@@ -4,39 +4,47 @@
 #[macro_use]
 extern crate user_lib;
 
-// Basic 测例中除去 mnt 以外的
-const TESTS: &[&str] = &[
-    "dup2\0",
-    "clone\0",
-    "execve\0",
-    "exit\0",
-    "fork\0",
+/*
+
+    "execve\0",c'd
     "getpid\0",
     "gettimeofday\0",
-    "wait\0",
-    "waitpid\0",
     "write\0",
+    "clone\0",
+    "fork\0",
     "yield\0",
+    "wait\0",
+    "exit\0",
+    "waitpid\0",
+    "getppid\0", 
+    "sleep\0",
+    "brk\0", 
+    "times\0",
+    
+*/
 
-    "brk\0", // 214
+// Basic 测例中除去 mnt 以外的
+const TESTS: &[&str] = &[
+
+    "uname\0", // 160
+    "mmap\0", // panic
+    "munmap\0", // panic
+ 
+
     "chdir\0", // 34
     "close\0", // panic
     "dup\0", // 23
+    "dup2\0",//fatal
     "fstat\0", // fatal
     "getcwd\0", // 17
     "getdents\0", // 61
-    "getppid\0", // error
     "mkdir_\0", // 34
-    "mmap\0", // panic
     "mount\0", // 40
-    "munmap\0", // panic
     "openat\0", // panic
     "open\0", // fatal
     "pipe\0", // waiting forever
     "read\0", // fatal
-    "times\0", // 153
     "umount\0", // panic
-    "uname\0", // 160
     "unlink\0", // panic
 ];
 
@@ -57,7 +65,7 @@ pub fn main() -> i32 {
             pids[i] = pid;
         }
         let mut xstate: i32 = Default::default();
-        let wait_pid = waitpid(pids[i] as usize, &mut xstate);
+        let wait_pid = waitpid(pids[i] as usize, &mut xstate,0);
         assert_eq!(pids[i], wait_pid);
         println!(
             "\x1b[32mUsertests: Test {} in Process {} exited with code {}\x1b[0m",
